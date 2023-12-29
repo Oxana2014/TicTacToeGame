2d74ba245530f6c46408367d7ab750f61302357a
Oksana Kormilina <keysb@mail.ru>
2023-12-29 16:16:15 +0400

    Add GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Player initialName="Player 2" symbol="O" />
      </ol>
 
-     GAME BOARD
+     <GameBoard></GameBoard>
     </div>
 
     LOG
