6adfb8f233ffa2ea7589698635c61cfe55b498a1
Oksana Kormilina <keysb@mail.ru>
2024-01-02 17:47:21 +0400

    Disable pressed squares

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,40 +1,41 @@
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, turns}) {
-let gameBoard = initialGameBoard
-for (const turn of turns) {
-   const {square, player} = turn
-   const {row, col} = square
-   gameBoard[row][ col] = player
-}
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
 
-//   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-//   function handleSelectSquare(rowIndex, colIndex) {
-//     setGameBoard((prevGameBoard) => {
-//       const updatedBoard = [
-//         ...prevGameBoard.map((innerArray) => [...innerArray]),
-//       ];
-//       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-//       return updatedBoard;
-//     });
-//     onSelectSquare()
-//   }
+  //   function handleSelectSquare(rowIndex, colIndex) {
+  //     setGameBoard((prevGameBoard) => {
+  //       const updatedBoard = [
+  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //       ];
+  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //       return updatedBoard;
+  //     });
+  //     onSelectSquare()
+  //   }
 
   return (
     <ol id="game-board">
-      {
-      gameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
